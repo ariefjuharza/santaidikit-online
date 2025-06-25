8b4b94c8d65993e64f63bb60e24a31ee3cdd996a
BONO DIGITAL STUDIO <personal.spirit.mandiri@gmail.com>
2025-06-26 03:01:31 +0700

    Integrasi awal Sveltia CMS

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './layouts/**/*.html',
+    './content/**/*.{html,md}',
+    './themes/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        firacode: ['"Fira Code"', 'monospace'],
+        sans: ['"Plus Jakarta Sans"', 'ui-sans-serif', 'system-ui'],
+      },
+    },
+  },
+  plugins: [],
+}
